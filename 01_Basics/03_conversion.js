a41bcf50daa4b2bf1da707c17bdc5719529aefaa
Vaishnavi Moharekar <moharekarvaishnavi4010@gmail.com>
2024-03-22 16:56:16 +0000

    conversion

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,13 @@
+let score = "24vrm"
+//let score =  null
+console.log(typeof score)
+let val = Number(score)
+console.log(val)
+console.log(typeof val)
+
+//if we convert "24" => prints value as 24 and datatype as number
+// if "24vrm" => NaN => prints value as 24 but datatype is "Not a Number(NaN)" 
+
+let isLogged = 1
+let BooleanIsLogged = Boolean(isLogged)
+console.log(BooleanIsLogged)
