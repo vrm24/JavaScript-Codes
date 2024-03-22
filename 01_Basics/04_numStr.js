946e856099412343e00fd6ef2788c76a76b4f1af
Vaishnavi Moharekar <moharekarvaishnavi4010@gmail.com>
2024-03-22 17:42:30 +0000

    numStr

---
+++ 01_Basics/04_numStr.js
@@ -0,0 +1,12 @@
+let str1 = "hi"
+let str2 = "vrm"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+console.log(+true)
+console.log(+"")
