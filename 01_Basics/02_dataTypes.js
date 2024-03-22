784e02b083a4fccde99cdea070d697b7af7e192d
Vaishnavi Moharekar <moharekarvaishnavi4010@gmail.com>
2024-03-22 13:50:24 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as newer version
+
+//alert(2+3) //we are using nodejs not browser
+
+//when to use semicolon?
+//console.log(3+4) console.log(2-4) // here it will give error
+
+/*JavaScript uses Automatic Semicolon Insertion (ASI) to determine where semicolons should be inserted in code. ASI works by looking for certain patterns in code, such as line breaks and closing curly braces. If ASI finds a pattern that indicates the end of a statement, it will insert a semicolon automatically. */
+
+//DataTypes
+//number => 2^53
+//bigint
+//string
+//boolean => true/false4
+//null => standalone value
+//undefined => value is not assigned yet
+//symbol => unique  (used in react)
+
+//object => 
+
+console.log(typeof undefined);
+console.log(typeof null)
+//null is an object
+//undefined is a data type
