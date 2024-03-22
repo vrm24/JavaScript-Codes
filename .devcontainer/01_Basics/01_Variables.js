890bd8098efc0b02441fd650f0f25e1405ddc867
Vaishnavi Moharekar <moharekarvaishnavi4010@gmail.com>
2024-03-22 13:39:37 +0000

    basics start

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accId = 23141
+let accMail = "vrm24@gmail.com"
+var accPass = "heloo"
+accCity="shirala"
+let accState
+//accId = 2
+
+accMail = "wew@gmail.com"
+accPass = "wrwe";
+accCity = "pune"
+console.log(accId);
+console.table([accId, accMail, accPass, accCity, accState])
+
+/*
+    prefer not use var due to issue of scope
+*/ 
