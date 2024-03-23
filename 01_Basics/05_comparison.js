feabf2429f134b2bfe8d687c90b7415dd9b48d62
Vaishnavi Moharekar <moharekarvaishnavi4010@gmail.com>
2024-03-23 05:16:27 +0000

    comparison

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,22 @@
+//console.log(2>1);
+// console.log("2">1)
+// console.log("2">"1")
+
+// console.log("02">"1")
+// console.log("02">1)
+
+// console.log(null>0)
+// console.log(null==0) 
+// console.log(null>=0)
+// console.log(null<=0)
+/*  equality and >, < works differently 
+    commparisons convert null to a number, treating it as 0.
+    That's why null>=0 is true and null>0 is false
+*/
+
+// console.log(undefined ==0)
+// console.log(undefined <0)
+// console.log(undefined >0)
+
+console.log("2"==2)
+console.log("2"===2)
