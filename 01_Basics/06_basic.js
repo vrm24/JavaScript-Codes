48d27d4b7ee3cf258212c04a7faabde01f49aa36
Vaishnavi Moharekar <moharekarvaishnavi4010@gmail.com>
2024-03-24 04:05:43 +0000

    basic

---
+++ 01_Basics/06_basic.js
@@ -0,0 +1,7 @@
+// var fname = "vaishnavi"      
+// var fname = "shreesha"       //no error
+
+let fname = "vaishnavi"
+fname = "shreesha"
+//let fname = "shreesha"  //gives error
+console.log(fname)
