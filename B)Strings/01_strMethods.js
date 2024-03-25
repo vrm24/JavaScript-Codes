95ff41a74d7e5fc9412a55871cd233402f4f983c
Vaishnavi Moharekar <moharekarvaishnavi4010@gmail.com>
2024-03-25 15:03:25 +0000

    string

---
+++ B)Strings/01_strMethods.js
@@ -0,0 +1,16 @@
+//let fname = " vaishnavi ";
+
+//console.log(fname[3])
+//fname = fname.trim()    //removes spaces
+// console.log(fname)
+
+// fname.trim()
+// console.log(fname.length)
+
+let fname = "vaishnavi"
+
+// console.log(fname.toUpperCase())
+// console.log(fname.toLowerCase())
+
+let newstr = fname.slice(0,5)
+console.log(newstr)
---
+++ B)Strings/02_strConcat.js
@@ -0,0 +1,8 @@
+// let str1 = "vaishnavi"
+// let str2 = "moharekar"
+
+let str1 = "129"
+let str2 = "23"
+let fullname = +str1 + +str2    // to convert str into number put + before string
+console.log(fullname)
+
